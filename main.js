00af555f6c3ee16df4b328b432796d66119aa8a3
VladyslavHeranin <60896490+VladyslavHeranin@users.noreply.github.com>
2020-08-10 13:45:10 +0300

    Add files via upload
    unique array elements

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-function hello(name){
-    const massage = "hello " + name;
-    return function(){
-        console.log(massage);
+let first = [1, 2, 2, -3, 34, -5, 6, 0];
+
+let second = [9, 6, 7, 4, 5, 1, 23, 4, 0, 5, 4, -3];
+
+let result = []
+
+for (let i = 0; i < first.length; i++) {
+
+    for (let j = 0; j < second.length; j++) {
+
+        if (first[i] === second[j]) {
+            result.push(second[j])     
+        }else{
+            continue ;
+        }
+   
     }
+
 }
- 
-let max = hello('max');
- 
-max()
+   console.log(result)
+
