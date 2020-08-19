621b38d5444500dbb4152cd01f29d9b448eb8fdd
VladyslavHeranin <60896490+VladyslavHeranin@users.noreply.github.com>
2020-08-19 15:28:51 +0300

    Add files via upload

---
+++ AfterBefore.js
@@ -0,0 +1,14 @@
+
+var str = 'Астрономия — Наука о небесных телах';
+
+function strin(lock, simb, str) {
+    if (lock === "after") {
+        console.log(str.slice(str.indexOf(simb)))
+    } if (lock === "before") {
+        console.log(str.slice(0, str.indexOf(simb)))
+    } else {
+        return str
+    }
+}
+
+strin("before", "Н", str)
