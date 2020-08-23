75d6ce51765d6ac874eea111ad3c44afc8b748a9
VladyslavHeranin <60896490+VladyslavHeranin@users.noreply.github.com>
2020-08-23 22:57:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let string = 'cool';
+
+let subString = 'o';
+
+function srt(string, subString) {
+    result = string.indexOf(subString);
+    if (result === -1) {
+        console.log(false)
+    } else {
+        console.log(true)
+    }
+}
+ 
